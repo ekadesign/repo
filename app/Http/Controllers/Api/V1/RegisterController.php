44ecb6ef5e28c7ba1400f2c9a37eba31e164ff47
Konstantin Efimov <ekadesign@bk.ru>
2018-04-18 20:25:50 +0300

    minor registration fixes

--- app/Http/Controllers/Api/V1/RegisterController.php
+++ app/Http/Controllers/Api/V1/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Repositories\UserRepository;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
